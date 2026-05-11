ac613f8465442f3bb26ff0ec005a89e26c81c24c
stellarjmr <stellarjmr@users.noreply.github.com>
2026-05-11 17:19:41 +0100

    bloom 0.6.0

--- Formula/bloom.rb
+++ Formula/bloom.rb
@@ -1,15 +1,15 @@
 class Bloom < Formula
   desc "Config-driven terminal updater for developer tools"
   homepage "https://github.com/stellarjmr/bloom"
-  version "0.5.15"
+  version "0.6.0"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/stellarjmr/bloom/releases/download/v0.5.15/bm-darwin-arm64.tar.gz"
-    sha256 "156bda829a2c8ac53bc473635cf07c6f370b663bd049ab31eb5240a6aaaf0c31"
+    url "https://github.com/stellarjmr/bloom/releases/download/v0.6.0/bm-darwin-arm64.tar.gz"
+    sha256 "2dcd7eef13f047bbceb41350568873759bb3ffc1b0583a8a593c455db581d3f6"
   else
-    url "https://github.com/stellarjmr/bloom/releases/download/v0.5.15/bm-darwin-amd64.tar.gz"
-    sha256 "2fd60edeffb5ca6f19e8c3bdd974839716ea958a56eff01060f2a47792b5e627"
+    url "https://github.com/stellarjmr/bloom/releases/download/v0.6.0/bm-darwin-amd64.tar.gz"
+    sha256 "ce60b5ede1643469322be6de9686780270e3dd23dabd5480a11e36001afb77f8"
   end
 
   def install
