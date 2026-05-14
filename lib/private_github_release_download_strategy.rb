b6e9c9e63410b27c09d17ee51cf8f4a01d716786
stellarjmr <219479939+stellarjmr@users.noreply.github.com>
2026-05-14 15:30:44 +0100

    Allow pawd formula to use gh auth token

--- lib/private_github_release_download_strategy.rb
+++ lib/private_github_release_download_strategy.rb
@@ -52,7 +52,7 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < CurlDownloadStrategy
   def set_github_token
     @github_token = ENV["HOMEBREW_GITHUB_API_TOKEN"].to_s
     @github_token = ENV["GITHUB_TOKEN"].to_s if @github_token.empty?
-    @github_token = `gh auth token 2>/dev/null`.strip if @github_token.empty?
+    @github_token = gh_auth_token if @github_token.empty?
 
     if @github_token.empty?
       raise CurlDownloadStrategyError,
@@ -60,5 +60,14 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < CurlDownloadStrategy
     end
 
     ENV["HOMEBREW_GITHUB_API_TOKEN"] = @github_token
+  end
+
+  def gh_auth_token
+    ["gh", "/opt/homebrew/bin/gh", "/usr/local/bin/gh"].each do |gh|
+      token = `#{gh} auth token 2>/dev/null`.strip
+      return token unless token.empty?
+    end
+
+    ""
   end
 end
