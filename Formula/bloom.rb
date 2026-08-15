aabaebbca6cf797e0d7fa92ad2aed492488c8c9c
stellarjmr <219479939+stellarjmr@users.noreply.github.com>
2026-08-15 15:14:57 +0100

    bloom 0.6.24

--- Formula/bloom.rb
+++ Formula/bloom.rb
@@ -4,11 +4,11 @@ class Bloom < Formula
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/stellarjmr/bloom/releases/download/v0.6.23/bm-darwin-arm64.tar.gz"
-    sha256 "9f5d4f97c54b081d158ed7b3d67d5cef605afeb863fe474e3a5dd5d22b7f574b"
+    url "https://github.com/stellarjmr/bloom/releases/download/v0.6.24/bm-darwin-arm64.tar.gz"
+    sha256 "69f9445ccaae108d3458565ddf4de458cdfe10e02c037e9e22f3b8ee865c84d9"
   else
-    url "https://github.com/stellarjmr/bloom/releases/download/v0.6.23/bm-darwin-amd64.tar.gz"
-    sha256 "c19ad259d53f8694228f8f793ed8c7b7daa04464a9e94258bd6c1424d78dee96"
+    url "https://github.com/stellarjmr/bloom/releases/download/v0.6.24/bm-darwin-amd64.tar.gz"
+    sha256 "01ba2b05e71bcce66df7fdfc12598ec590c57adaf43bd16fa0c8d93395f803e3"
   end
 
   def install
